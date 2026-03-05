efd4000463d70128d3b081f135fa997d63f84aa4
Vedant-Walke <vedantwalke0083@gmail.com>
2026-03-05 11:53:54 +0530

    Added header

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,65 @@
+import assets from "@/lib/assets"
+
+const Header = () => {
+
+    const links = [
+        {
+            label: "Products",
+            href: "/products",
+        },
+        {
+            label: "Solutions",
+            href: "/solutions",
+        },
+        {
+            label: "Customers",
+            href: "/customers",
+        },
+        {
+            label: "Pricing",
+            href: "/pricing",
+        },
+    ]
+
+    return (
+        <header
+            className="border sticky top-0 left-0 right-0 mt-6 min-h-12 flex items-center justify-between"
+        >
+            <div className="flex items-center">
+                <a href="/" className="aspect-square h-12 flex items-center justify-center">
+                    <img
+                        src={assets.logo.icon}
+                        alt="Owlysis Logo"
+                        className="size-8"
+                    />
+                </a>
+                {links.map((value) => (
+                    <a
+                        key={value.label}
+                        href={value.href}
+                        className="flex h-12 items-center justify-center px-3 border border-y-transparent border-r-transparent last:border-r-border hover:border-primary transition"
+                    >
+                        {value.label}
+                    </a>
+                ))}
+            </div>
+            <div className="flex items-center">
+                    <a
+                        href="/contact-us"
+                        className="flex h-12 items-center justify-center px-3 border border-y-transparent border-r-transparent last:border-r-border hover:border-primary transition"
+                    >
+                        Contact us
+                    </a>
+                    <a
+                        target="_blank"
+                        href="https://app.owlysis.com"
+                        className="flex h-12 items-center justify-center px-3 bg-primary text-primary-foreground"
+                    >
+                        Login
+                    </a>
+            </div>
+        </header>
+    )
+}
+
+export { Header }
---
+++ src/lib/assets.ts
@@ -0,0 +1,6 @@
+export default {
+    logo: {
+        icon: "https://api.s3.owlysis.com/owlysis/owlysis-logo-icon.svg",
+        text: "https://api.s3.owlysis.com/owlysis/owlysis-logo-text.svg"    
+    }
+}
